b01013705532809311e11657cfe9ea2a521a41b9
tueda <ueo@ueo.co.jp>
2020-10-05 15:30:23 +0900

    maven repository for apache poi 4.1.2 adapted is moved to gh-pages-dev.

--- blanco/main/blanco/stringgroup/BlancoStringGroupConstants.java
+++ blanco/main/blanco/stringgroup/BlancoStringGroupConstants.java
@@ -20,7 +20,7 @@ public class BlancoStringGroupConstants {
      * 項目番号:3<br>
      * バージョン番号。
      */
-    public static final String VERSION = "0.4.0";
+    public static final String VERSION = "2.1.1";
 
     /**
      * 項目番号:4<br>
